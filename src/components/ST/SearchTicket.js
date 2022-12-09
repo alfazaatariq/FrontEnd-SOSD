3c43d53844424bdf7ebd22042cbe384325638310
alfazaatariq <m.alfazaatariq@gmail.com>
2022-12-09 17:53:52 +0700

    searchTicket bug fixed

--- src/components/ST/SearchTicket.js
+++ src/components/ST/SearchTicket.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { getTicketsByForm, getTujuan, getAsal } from '../../utils/functions';
 import { useSearchParams } from 'react-router-dom';
 import { DataContext } from '../../App';
 import { TicketList } from '../TL/TicketList';
-import {MdDirectionsBoatFilled} from 'react-icons/md'
-import {MdLocationPin} from 'react-icons/md'
-import {MdOutlineDateRange} from 'react-icons/md'
+import { MdDirectionsBoatFilled } from 'react-icons/md';
+import { MdLocationPin } from 'react-icons/md';
+import { MdOutlineDateRange } from 'react-icons/md';
 import './ST.css';
 
 const SearchTicket = () => {
@@ -41,86 +41,92 @@ const SearchTicket = () => {
   }
 
   return (
-    <div className="cardDiv grid">
-      <form className='forms'
+    <div className='cardDiv grid'>
+      <form
+        className='forms'
         action='#'
         onSubmit={(e) => {
           e.preventDefault();
           onClickHandler();
         }}
       >
-
-      <div className='fromInput'> 
-        <label htmlFor='city'>Pilih Pelabuhan Awal:</label>
-          <div className="input flex">
+        <div className='fromInput'>
+          <label htmlFor='city'>Pilih Pelabuhan Awal:</label>
+          <div className='input flex'>
             <select
-            onChange={async (e) => {
-              setTujuan(await getTujuan(e.target.value));
-              setSelectedAsal(e.target.value);
-              setSearchParams({ asal: e.target.value });
-            }}
-            required
-          >
-            <option value=''>Select here...</option>
+              onChange={async (e) => {
+                setTujuan(await getTujuan(e.target.value));
+                let dropDown = document.getElementById('tujuan');
+                dropDown.selectedIndex = 0;
+                setSelectedTujuan('');
+                setSelectedAsal(e.target.value);
+                setSearchParams({ asal: e.target.value });
+              }}
+              required
+            >
+              <option value=''>Select here...</option>
 
-            {asal.map((item) => {
-              return (
-                <option key={item.key} value={item}>
-                  {item}
-                </option>
-              );
-            })}
-          </select>
-          <MdDirectionsBoatFilled className='icon'/>
+              {asal.map((item) => {
+                return (
+                  <option key={item.key} value={item}>
+                    {item}
+                  </option>
+                );
+              })}
+            </select>
+            <MdDirectionsBoatFilled className='icon' />
+          </div>
         </div>
-      </div>
 
-      <div className='destinationInput'>
-        <label htmlFor='city'>Pilih Pelabuhan Tujuan:</label>
-        <div className="input flex">
-          <select
-            onChange={(e) => {
-              setSelectedTujuan(e.target.value);
-              setSearchParams({
-                asal: searchParams.get('asal'),
-                tujuan: e.target.value,
-              });
-            }}
-            required
-          >
-            <option value=''>Select here...</option>
-            {tujuan.map((item) => {
-              return (
-                <option key={item.key} value={item}>
-                  {item}
-                </option>
-              );
-            })}
-          </select>
-          <MdLocationPin className='icon'/>
+        <div className='destinationInput'>
+          <label htmlFor='city'>Pilih Pelabuhan Tujuan:</label>
+          <div className='input flex'>
+            <select
+              id='tujuan'
+              onChange={(e) => {
+                setSelectedTujuan(e.target.value);
+                setSearchParams({
+                  asal: searchParams.get('asal'),
+                  tujuan: e.target.value,
+                });
+              }}
+              required
+            >
+              <option value=''>Select here...</option>
+              {tujuan.map((item) => {
+                return (
+                  <option key={item.key} value={item}>
+                    {item}
+                  </option>
+                );
+              })}
+            </select>
+            <MdLocationPin className='icon' />
+          </div>
         </div>
-      </div>
 
-      <div className='deteInput'>
-        <label htmlFor='city'>Pilih Tanggal Berangkat:</label>
-        <div className="input flex">
-          <input
-            type='date'
-            onChange={(e) => {
-              setTanggal(e.target.value);
-              setSearchParams({
-                asal: searchParams.get('asal'),
-                tujuan: searchParams.get('tujuan'),
-                tanggal: e.target.value,
-              });
-            }}
-            required
-          />
-          <MdOutlineDateRange className='icon'/>
+        <div className='deteInput'>
+          <label htmlFor='city'>Pilih Tanggal Berangkat:</label>
+          <div className='input flex'>
+            <input
+              type='date'
+              onChange={(e) => {
+                setTanggal(e.target.value);
+                setSearchParams({
+                  asal: searchParams.get('asal'),
+                  tujuan: searchParams.get('tujuan'),
+                  tanggal: e.target.value,
+                });
+              }}
+              required
+            />
+            <MdOutlineDateRange className='icon' />
+          </div>
         </div>
-      </div>
 
-      <button className='btnSearch' type='submit'>Cari Tiket</button>
+        <button className='btnSearch' type='submit'>
+          Cari Tiket
+        </button>
       </form>
     </div>
   );
