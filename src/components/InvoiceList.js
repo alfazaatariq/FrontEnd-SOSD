7c11e2cf17e00faf37ce6e76c9ec70abe0cd2548
alfazaatariq <m.alfazaatariq@gmail.com>
2022-12-09 18:17:57 +0700

    change invoicelist list

--- src/components/InvoiceList.js
+++ src/components/InvoiceList.js
@@ -28,19 +28,19 @@ export const InvoiceList = () => {
             >
               <ul className='list-ticket' key={item.key}>
                 <h1>Invoice</h1>
+                <li>order_id : {item.order_id}</li>
                 <li>user_id : {item.user_id}</li>
                 <li>asal : {item.asal}</li>
                 <li>tujuan : {item.tujuan}</li>
                 <li>tanggal : {item.tanggal}</li>
                 <li>waktu_berangkat : {item.waktu_berangkat}</li>
                 <li>waktu_tiba : {item.waktu_tiba}</li>
-                <li>harga : {item.harga}</li>
                 <li>nama : {item.nama}</li>
                 <li>no : {item.no}</li>
                 <li>alamat : {item.alamat}</li>
+                <li>harga : {item.harga}</li>
                 <li>jumlah_tiket : {item.jumlah_tiket}</li>
                 <li>total_harga : {item.total_harga}</li>
-                <li>order_id : {item.order_id}</li>
               </ul>
             </Link>
           );
